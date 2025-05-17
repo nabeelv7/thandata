9275fa2816422fe37d7664d6ca0195dfe365aaeb
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-05-17 14:31:23 +0500

    added a lot of things

---
+++ src/routes/api/visit/+server.js
@@ -0,0 +1,7 @@
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+  const data = await request.json();
+  console.log(data);
+
+  return new Response();
+}
