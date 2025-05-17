f3e04ef34858b729001a35a7c7fd08be77447548
nabeelv7 <nabeelsadiq.dev@gmail.com>
2025-05-17 20:54:02 +0500

    maybe fixed

--- src/auth.js
+++ src/auth.js
@@ -8,8 +8,9 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [GitHub],
   trustedHosts: [
     "thandata.vercel.app", // your production domain, no https://
-    "https://thandata-dpsr6vgsf-nabeelv7s-projects.vercel.app/",
-    "https://thandata-4yi9qq8tt-nabeelv7s-projects.vercel.app",
+    "localhost:4173", // your dev server, optional
     "localhost:5173", // your dev server, optional
   ],
+
+  trustHost: true,
 });
